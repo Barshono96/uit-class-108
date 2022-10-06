9cce320e5f971d33ce58f223b550f1c47d02f272
Barshono96 <88356901+Barshono96@users.noreply.github.com>
2022-10-07 00:45:19 +0600

    first commit

---
+++ class-1p.py
@@ -0,0 +1,17 @@
+print ('hello')
+
+name='shifat'
+print(name)
+num=4/2
+num2=str(num)
+print(type(num))
+print(type(num2))
+#Check comments
+if name=='BARSHON':
+     print('OK') 
+
+if name=='shifat':
+     print('not ok')
+
+     def myfunc():
+           'v'
